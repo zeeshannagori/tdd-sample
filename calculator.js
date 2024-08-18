7fe08937bf2c2ca0e882d4106eeeaac287939085
Zeeshan Nagori <zeeshannagori007@gmail.com>
2024-08-19 02:41:41 +0530

    test case for single input

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
 export function sum(str) {
   if (!str) return 0;
+  return parseInt(str);
 }
--- calculator.test.js
+++ calculator.test.js
@@ -4,3 +4,7 @@ import { sum } from "./calculator";
 test("return 0 if input is empty", () => {
   expect(sum("")).toBe(0);
 });
+
+test("return number 1 if input is string 1", () => {
+  expect(sum("1")).toBe(1);
+});
