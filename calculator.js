25f3099738d1655cd9db82e75564819e90f3273b
Zeeshan Nagori <zeeshannagori007@gmail.com>
2024-08-19 03:09:53 +0530

    test case for handling '\n' OR new line

--- calculator.js
+++ calculator.js
@@ -1,4 +1,14 @@
 export function sum(str) {
   if (!str) return 0;
-  return str.split(",").reduce((acc, cur) => +acc + +cur, 0);
+
+  const numbers = [];
+
+  str.split("").map((ele, i) => {
+    const val = +ele;
+    if (!isNaN(val) && typeof val === "number") {
+      numbers.push(val);
+    }
+  });
+
+  return numbers.reduce((acc, cur) => +acc + +cur, 0);
 }
--- calculator.test.js
+++ calculator.test.js
@@ -12,3 +12,7 @@ test("return number 1 if input is string 1", () => {
 test("return 6 if input is string '1,5'", () => {
   expect(sum("1,5")).toBe(6);
 });
+
+test("return sum even-if new line is present in the input string", () => {
+  expect(sum("1\n2,3")).toBe(6);
+});
