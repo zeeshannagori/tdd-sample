71e65a361d8e78f1c35733daa70e6240179815bf
Zeeshan Nagori <zeeshannagori007@gmail.com>
2024-08-19 02:34:41 +0530

    test case for empty input

--- calculator.js
+++ calculator.js
@@ -1,3 +1,3 @@
-export function sum(a, b) {
-  return a + b;
+export function sum(str) {
+  if (!str) return 0;
 }
--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 import { sum } from "./calculator";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("return 0 if input is empty", () => {
+  expect(sum("")).toBe(0);
 });
